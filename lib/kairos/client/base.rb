f4d0ad380556c4a52256d8f2dcaf4ba79e3bd02a
Chris Allick <chrisallick@gmail.com>
2014-07-10 12:06:04 -0400

    cleaning fixed version

--- lib/kairos/client/base.rb
+++ lib/kairos/client/base.rb
@@ -9,25 +9,25 @@ module Kairos
       
       attr_accessor :app_id, :app_key
 
-      #{
-      #  twitter_username => [twitter screen name]
-      #  twitter_password => [twitter password]
-      #}
-      attr_accessor :twitter_credentials
+      # #{
+      # #  twitter_username => [twitter screen name]
+      # #  twitter_password => [twitter password]
+      # #}
+      # attr_accessor :twitter_credentials
 
-      #{
-      #  twitter_oauth_user => [twitter OAuth user]
-      #  twitter_oauth_secret => [twitter OAuth secret]
-      #  twitter_oauth_token => [twitter OAuth token]
-      #}
-      attr_accessor :twitter_oauth_credentials
+      # #{
+      # #  twitter_oauth_user => [twitter OAuth user]
+      # #  twitter_oauth_secret => [twitter OAuth secret]
+      # #  twitter_oauth_token => [twitter OAuth token]
+      # #}
+      # attr_accessor :twitter_oauth_credentials
 
-      #{
-      #  fb_user => [facebook user id]
-      #  fb_session => [facebook session id]
-      #  fb_oauth_token =>  [facebook oauth 2.0 access token]
-      #}
-      attr_accessor :facebook_credentials
+      # #{
+      # #  fb_user => [facebook user id]
+      # #  fb_session => [facebook session id]
+      # #  fb_oauth_token =>  [facebook oauth 2.0 access token]
+      # #}
+      # attr_accessor :facebook_credentials
 
       include Kairos::Client::Utils
       include Kairos::Client::Recognition
